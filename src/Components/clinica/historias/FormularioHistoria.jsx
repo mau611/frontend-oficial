148a41fc589ce109bab3fc7e05cea49f4a4789e5
Sabrinamonti <sabrinamontic@gmail.com>
2023-07-04 05:21:44 -0400

    initial changes

--- src/Components/clinica/historias/FormularioHistoria.jsx
+++ src/Components/clinica/historias/FormularioHistoria.jsx
@@ -1,8 +1,24 @@
+import { Suspense } from 'react'
 import React from 'react'
+import { fetchData } from '../../../js/FetchData'
+
+const apiData = fetchData("https://stilettoapi.com/api/consultas")
 
 const FormularioHistoria = () => {
+  const data = apiData.leer();
+  console.log(data)
   return (
-    <div>Formulario Historia</div>
+    <div>
+      Formulario Historias
+      <br />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <ul className='card'>
+          {data?.map((paciente)=>(
+            <li key={paciente.id}>{paciente.title}</li>
+          ))}
+        </ul>
+      </Suspense>
+    </div>
   )
 }
 
