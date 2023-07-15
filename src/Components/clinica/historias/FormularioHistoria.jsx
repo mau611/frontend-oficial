f3f4b45ca637a44aaf48a151b771194768c29fc5
mau611 <mauricio611rp@gmail.com>
2023-07-15 05:22:36 -0400

    historia

--- src/Components/clinica/historias/FormularioHistoria.jsx
+++ src/Components/clinica/historias/FormularioHistoria.jsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Suspense, useState } from "react";
 import { fetchData } from "../../../scripts/FetchData";
 import { Autocomplete, TextField } from "@mui/material";
+import axios from "axios";
+import { useEffect } from "react";
 
-const apiData = fetchData("https://stilettoapi.com/api/pacientes");
+
+const endpoint = "https://stilettoapi.com/api";
 
 const FormularioHistoria = () => {
-  const [paciente, setPaciente] = useState(null);
-  const pacientes = apiData.leer();
+  const dataFetchedRef = useRef(false);
+  const clickRef = useRef(null);
+  const [paciente, setPaciente] = useState([]);
+  const [pacientes,setPacientes] = useState([]);
+  const [citas,setCitas] = useState([])
+  const [cita,setCita] = useState("")
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    getPacientes();
+    window.clearTimeout(clickRef?.current);
+  }, []);
+  
+
+  const getPacientes = async () => {
+    const response = await axios.get(`${endpoint}/pacientes`);
+    setPacientes(response.data)
+  };
+
+  const getCitas = async (value) => {
+    setCitas([])
+    setCita()
+    setPaciente(value)
+    const id = value.split(" ")[0]
+    const response = await axios.get(`${endpoint}/paciente/${id}`);
+    setCitas(response.data.citas)
+  };
   return (
     <div>
-      Formulario Historias
+      Seleccione un paciente
       <br />
       <Autocomplete
         required
         freeSolo
         id="pacientes"
         value={paciente}
-        onChange={(e, value) => setPaciente(value)}
+        onChange={(e, value) => getCitas(value)}
         disableClearable
         options={pacientes.map(
           (option) =>
@@ -36,7 +65,30 @@ const FormularioHistoria = () => {
       />
       <br />
       <div>
-        {paciente}
+        Seleccione una Fecha
+        <br />
+        <Autocomplete
+        required
+        freeSolo
+        id="citas"
+        value={cita}
+        onChange={(e, value) => setCita(value)}
+        disableClearable
+        options={citas.map(
+          (option) =>
+            option.id + " - Fecha de la cita:  " + ((new Date(""+option.start).toLocaleDateString('en-GB')))
+        )}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Seleccione la cita"
+            InputProps={{
+              ...params.InputProps,
+              type: "search",
+            }}
+          />
+        )}
+      />
       </div>
     </div>
   );
