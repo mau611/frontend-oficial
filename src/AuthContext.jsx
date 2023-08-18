2befdd5b46a89c45d78b7c6a6b855cce025ab33a
mau611 <mauricio611rp@gmail.com>
2023-08-18 18:13:04 -0400

    login

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -25,7 +25,7 @@ export const AuthProvider = ({ children }) => {
 
   // csrf token generation for guest methods
   const csrfToken = async () => {
-    await axios.get("http://localhost:8000/sanctum/csrf-cookie");
+    await axios.get("https://stilettoapi.com/sanctum/csrf-cookie");
     return true;
   };
 
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://stilettoapi.com/api",
+
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
