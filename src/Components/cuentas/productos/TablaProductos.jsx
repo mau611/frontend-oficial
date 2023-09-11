ab5b6c1a395a6c57e480056bce276b25cfa1c4c6
Sabrinamonti <sabrinamontic@gmail.com>
2023-09-11 14:48:17 -0400

    enlaces

--- src/Components/cuentas/productos/TablaProductos.jsx
+++ src/Components/cuentas/productos/TablaProductos.jsx
@@ -13,7 +13,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
-const endpoint = "http://localhost:8000/api";
+const endpoint = "http://scapi.shantispawellnesslife.com/api";
 
 const TablaProductos = () => {
   const [ventas, setVentas] = useState([]);
